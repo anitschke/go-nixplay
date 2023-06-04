f81ae280ebc33cca902928a1d6b38a583c6aaaa0
Andy Nitschke <andrew.b.nitschke@gmail.com>
2023-06-03 22:47:15 -0400

    WIP adding better errors

--- internal/errorx/wrap_if_error.go
+++ internal/errorx/wrap_if_error.go
@@ -6,12 +6,17 @@ import (
 	"runtime"
 )
 
+// WrapIfError wraps the provided error with an additional message if it is not
+// nil. This is intended to be used inside a defer to wrap the returned error
 func WrapIfError(msg string, err *error) {
 	if *err != nil {
 		*err = fmt.Errorf("%s: %w", msg, *err)
 	}
 }
 
+// WrapIfError wraps the provided error with the name of the caller if the error
+// is not nil. This is intended to be used inside a defer to wrap the returned
+// error
 func WrapWithFuncNameIfError(err *error) {
 	if *err != nil {
 		pc, _, _, ok := runtime.Caller(1)
